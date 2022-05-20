2b8ad0e011e2f0144e4df96d150829fa2b67adde
Pavel Demkovich <finn@finnix.servebeer.com>
2022-05-20 23:11:25 +0300

    final solution

--- square.go
+++ square.go
@@ -4,19 +4,27 @@ type Point struct {
 	x, y int
 }
 
+func NewPoint(x int, y int) Point {
+	return Point{x: x, y: y}
+}
+
 type Square struct {
 	start Point
 	a     uint
 }
 
-func (receiver) End() Point {
-	// implement me
+func NewSquare(start Point, a uint) Square {
+	return Square{start: start, a: a}
 }
 
-func (receiver) Area() uint {
-	// implement me
+func (s Square) End() Point {
+	return Point{x: s.start.x + int(s.a), y: s.start.y + int(s.a)}
 }
 
-func (receiver) Perimeter() uint {
-	// implement me
+func (s Square) Area() uint {
+	return s.a * s.a
+}
+
+func (s Square) Perimeter() uint {
+	return s.a * 4
 }
